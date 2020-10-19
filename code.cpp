ade96a35cbd7df3b7e260d0855d8149f58037284
Alnotz <56512110+Alnotz@users.noreply.github.com>
2020-10-19 18:21:19 +0200

    Correction de code & ajout.
    Une ligne en plus pour indiquer le nom du programme.

--- code.cpp
+++ code.cpp
@@ -1,6 +1,7 @@
 #include <streamio>
 int main (int argc, char* argv[])
 {
-  std::cout << "Hello world ! << std::endl;
+  std::cout << "Hello world !" << std::endl;
+  std::cout << "The program name is " << argv[0] << "." << std::endl;
   return 0;
 }
