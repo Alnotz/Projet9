2a1aa01ab374bb5673f3f52c8749af33730c9326
Alnotz <fredh0001@net-c.fr>
2020-10-19 18:54:08 +0200

    Car c'est plus correct.

--- code.cpp
+++ code.cpp
@@ -1,7 +1,8 @@
 #include <streamio>
+#include <cstdlib>
 int main (int argc, char* argv[])
 {
   std::cout << "Hello world !" << std::endl;
   std::cout << "The program name is " << argv[0] << "." << std::endl;
-  return 0;
+  return EXIT_SUCCESS;
 }
